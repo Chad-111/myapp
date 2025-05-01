a634ead2972fa9bfd1c50945f0956eb12221c995
Logan Stewart <johnloganstewart@yahoo.com>
2025-05-01 16:08:49 -0500

    Trying something else

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -3,7 +3,7 @@ import pytest
 import bcrypt
 import sys
 import os
-sys.path.append("../")
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 print(os.getcwd())
 from app import app, db
 from app import User  
